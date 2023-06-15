e5b3594032712c408a6b50fb0dfc381b7bf55902
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-15 16:32:24 -0500

    implement each method on Mylist

--- my_list.rb
+++ my_list.rb
@@ -1,7 +1,14 @@
 class MyList
-  
-    def initialize(*args)
-      @list = args
-    end
+  def initialize(*args)
+    @list = args
+  end
 
+  def each
+    @list.each do |element|
+      yield element if block_given?
+    end
+  end
 end
+
+list = MyList.new(1, 2, 3, 4)
+puts list
